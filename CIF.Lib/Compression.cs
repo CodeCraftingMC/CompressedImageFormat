8b536adb0cdcf4d2305c970b3a3bf1fd3418fcb0
BuildTools <unconfigured@null.spigotmc.org>
2023-07-08 10:11:06 +0200

    Added a method to round to nearest 10

--- CIF.Lib/Compression.cs
+++ CIF.Lib/Compression.cs
@@ -53,5 +53,9 @@ namespace CIF.Lib
 
             return decompressed.ToArray();
         }
+        public static int RoundOff(this int i)
+        {
+            return ((int)Math.Round(i / 10.0)) * 10;
+        }
     }
 }
